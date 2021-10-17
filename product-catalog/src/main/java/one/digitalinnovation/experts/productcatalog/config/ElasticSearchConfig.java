7e6e94961eb83ab90c107b3a531ffed7c8c88018
Sandro Andrade <sandro.andrade@nitryx.com>
2021-10-17 16:31:13 -0300

    Finalização adicionado gateway, config server e um service discovery

--- product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/config/ElasticSearchConfig.java
+++ product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/config/ElasticSearchConfig.java
@@ -1,22 +1,24 @@
 package one.digitalinnovation.experts.productcatalog.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.ElasticsearchEntityMapper;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.EntityMapper;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "one.digitalinnovation.experts.productcatalog.repository")
-public class ElasticSearchConfig {
-//public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+public class ElasticSearchConfig extends AbstractElasticsearchConfiguration{
 
-    /*
     @Override
     public RestHighLevelClient elasticsearchClient() {
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
@@ -34,19 +36,6 @@ public class ElasticSearchConfig {
 
         return entityMapper;
     }
-*/
-    @Bean
-    public RestHighLevelClient client() {
-        ClientConfiguration clientConfiguration
-                = ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
-                .build();
 
-        return RestClients.create(clientConfiguration).rest();
-    }
 
-    @Bean
-    public ElasticsearchOperations elasticsearchTemplate() {
-        return new ElasticsearchRestTemplate(client());
-    }
 }
